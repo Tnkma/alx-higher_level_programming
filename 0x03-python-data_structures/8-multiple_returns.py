878c89065168d8304bf9e873cf28689470416d93
Tnkma <onwusilikegodswill@gamil.com>
2023-10-08 08:15:19 -0700

    multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+
+    length = len(sentence)
+    first_character = sentence[0]
+
+    if sentence == None:
+        first_character = None
+    else:
+        return length, first_character
