79fc412924d8891ec67ab68f9f24708a3e0bde8f
Tnkma <onwusilikegodswill@gamil.com>
2023-10-09 04:17:47 -0700

    fixed error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,13 @@ de_args = len(sys.argv) - 1
 if de_args == 0:
     print("{} arguments.".format(de_args))
 elif de_args >= 1:
-    print("{} arguments:".format(de_args))
-    for index, args in enumerate(sys.argv[1:], start=1):
-        print("{}: {}".format(index, args))
+
+    if de_args == 1:
+        print("{} argument:".format(de_args))
+        for index, args in enumerate(sys.argv[1:], start=1):
+            print("{}: {}".format(index, args))
+    else:
+        print("{} arguments:".format(de_args))
+        for index, args in enumerate(sys.argv[1:], start=1):
+            print("{}: {}".format(index, args))
+
