0985232967c876e55d72a3f749a0e1e1f4e2a236
Tnkma <onwusilikegodswill@gmail.com>
2023-10-13 22:56:31 +0100

    fixed pycodestyle

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,6 +3,6 @@
 
 def multiply_by_2(a_dictionary):
     create_dic = {}
-    for i in a_dictionary:
+    for key, value in a_dictionary:
         create_dic = a_dictionary[i] * 2
     return create_dic
