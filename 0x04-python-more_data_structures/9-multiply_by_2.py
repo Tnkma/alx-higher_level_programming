45491fad7d022a82d91258720df70aab1f672aa4
Tnkma <onwusilikegodswill@gmail.com>
2023-10-15 10:59:02 +0100

    multiply each value for  the keys by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,6 +3,5 @@
 
 def multiply_by_2(a_dictionary):
     create_dic = {}
-    for key, value in a_dictionary:
-        create_dic = a_dictionary[i] * 2
+    create_dic = {key: value*2 for key, value in a_dictionary.items()}
     return create_dic
