252d6f1aaa183d9a81586de0e582f161055af241
Tnkma <onwusilikegodswill@gmail.com>
2023-10-23 17:39:06 +0100

    raised errors for indexerror

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,8 +3,10 @@
 def safe_print_list_integers(my_list=[], x=0):
     cont = 0
     for m in range(x):
-        if isinstance(my_list[m], int):
+        try:
             print("{:d}".format(my_list[m]), end='')
             cont += 1
+        except (ValueError, TypeError):
+            continue
     print()
     return cont
