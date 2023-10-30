e253850bc385fac91fb365b432a5b2e87856130a
Tnkma <onwusilikegodswill@gmail.com>
2023-10-31 00:19:03 +0100

    added underscore

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ This program sets and retrieve
 class Rectangle:
 
     def __init__(self, width=0, height=0):
-        self.height = height
-        self.width = width
+        self.__height = height
+        self.__width = width
 
     @property
     def width(self):
