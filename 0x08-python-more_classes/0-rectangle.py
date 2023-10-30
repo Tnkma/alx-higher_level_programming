2a9d94e0eb1f04d4d23e4b31ee451c60280d7f50
Tnkma <onwusilikegodswill@gmail.com>
2023-10-30 17:21:58 +0100

    defines an empty rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+An empty class that defines a rectangle.
+"""
+
+
+class Rectangle:
+    def my_rectangle(self):
+        """
+        Defines an empty rectangle class
+        """
+        pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+Python object and class task still continue
