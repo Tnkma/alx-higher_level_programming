0ecf04fa651289ea01cc50141ac2dc7e92928732
Tnkma <onwusilikegodswill@gmail.com>
2023-10-30 17:33:26 +0100

    getting errors

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-"""
-An empty class that defines a rectangle.
-"""
-
 
 class Rectangle:
     def my_rectangle(self):
-        """
-        Defines an empty rectangle class
-        """
-        pass
