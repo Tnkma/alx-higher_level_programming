02f480b977d49b263a39b778f9e7dd45cdceaed6
Tnkma <onwusilikegodswill@gmail.com>
2023-10-30 23:30:48 +0100

    set height first in the instances

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ a private instance height and width
 class Rectangle:
 
     def __init__(self, width=0, height=0):
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
