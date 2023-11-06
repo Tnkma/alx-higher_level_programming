c40454c65b9bab3ee2ab12c8e0183bf68753ef2c
Tnkma <onwusilikegodswill@gmail.com>
2023-11-06 20:33:33 +0100

    return true or false if object is of class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,6 +14,6 @@ def inherits_from(obj, a_class):
     :a_class: the class to check.
     :return: either false or true.
     """
-    if isinstance(obj, object) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
