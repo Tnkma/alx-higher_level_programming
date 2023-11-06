517dc45b0cc8332fbae0c2614919a144862a7aa4
Tnkma <onwusilikegodswill@gmail.com>
2023-11-06 14:18:01 +0100

    a function that returns the list of properties and attributes

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A function that returns the list of available attributes
+and methods of an object.
+"""
+
+
+def lookup(obj):
+    """
+    A lookup class that returns the lists using a dir function
+    """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,4 @@
+#############################################
+      PYTHON INHERITANCE TASK
+This project and its task cover the python inheritance concept and how class and subclassess inherit each other.
+############################################
