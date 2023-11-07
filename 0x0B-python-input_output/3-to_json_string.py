20cf23aefe35cf23160ca13deb3d92e02b54015d
Tnkma <onwusilikegodswill@gmail.com>
2023-11-07 19:30:42 +0100

    returns the representation of str usinf json dumps

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""
+A function that returns JSON rep of a str
+"""
+
+
+def to_json_string(my_obj):
+    """
+    returns the json rep of obj
+    """
+    return json.dumps(my_obj)
