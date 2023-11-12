d197a075e8a74e1d7053c4da0e55f40a6aa135dd
Tnkma <onwusilikegodswill@gmail.com>
2023-11-12 01:03:23 +0100

    created the class Square getter and setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,7 +10,7 @@ class Square(Rectangle):
 
     def __str__(self):
         """Overrides the str method to return [Rectangle] (<id>) <x>/<y> - <width>/<height>."""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
     @property
     def size(self):
         return self.width
@@ -23,3 +23,20 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.width = size
         self.height = size
+
+    def update(self, *args, **kwargs):
+        """
+        assigns attributes to args
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        if kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
