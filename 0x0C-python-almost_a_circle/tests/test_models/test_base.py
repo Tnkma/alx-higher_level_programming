76484d268d31ba75bc7832c3e1d88ad8d1867b4f
Tnkma <onwusilikegodswill@gmail.com>
2023-11-14 00:05:01 +0100

    wrote the format for the test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -9,9 +9,12 @@ each methods
 class TestBase(unittest.TestCase):
 
     def test_init(self):
-        obj = Base(12)
-        self.assertEqual(obj.id, 12)
-
+        obj = Base()
+        checkh = Base()
+        result = Base(12)
+        self.assertEqual(obj.id, 1)
+        self.assertEqual(checkh.id, 2)
+        self.assertEqual(result.id, 12)
 
 if __name__ == '__main__':
     unittest.main()
