61edc99f40efa9c8795e96309d82ffa699d38e5b
Tnkma <onwusilikegodswill@gmail.com>
2024-01-09 23:44:39 +0100

    prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const args = process.argv[2];
+if (args !== undefined) {
+  const argValue = parseInt(args, 10);
+	if (!isNaN(argValue)) {
+	  for (let i = 0; i < argValue; i++) {
+	    let row = '';
+	     for (let j = 0; j < argValue; j++) {
+		  row += 'X';
+	     }
+	       console.log(row);
+	  }
+	} else {
+	  console.log('Missing size');
+	}
+} else {
+  console.log('Missing size');
+}
