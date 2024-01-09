f58280ee0eb0f678ff5864f84b78310f7b75ae0b
Tnkma <onwusilikegodswill@gmail.com>
2024-01-09 15:20:43 +0100

    prints the command line argumnets

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-
+const cmdargs = args[0];
 if (args < 2) {
   console.log('No arguments');
 } else {
-  console.log(args);
+  console.log(cmdargs);
 }
