29ae099d03d196b26595a45d5332d029798afba2
Tnkma <onwusilikegodswill@gmail.com>
2024-01-09 21:28:37 +0100

    prints using the for loops

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const arrayStrings = [
+  'C is fun',
+  'Python is cool',
+  'JaveScript is amazing'
+];
+for (let i = 0; i < arrayStrings.length; i++) {
+  console.log(arrayStrings[i]);
+}
