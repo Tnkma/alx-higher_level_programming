b6360e79b3a442de0c9c4d480eed0eca3ab22e31
Tnkma <onwusilikegodswill@gmail.com>
2024-01-09 14:51:03 +0100

    fixed the length of the args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 
-if (args < 2) {
+if (args.length < 1) {
   console.log('No argument');
-} else {
+} else if (args.length == 1) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
