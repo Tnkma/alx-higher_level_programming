a77250027f75b23c38a6b94510fbd56050d86a7a
Tnkma <onwusilikegodswill@gmail.com>
2024-01-09 21:13:04 +0100

    prints args converted to interger

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const args = process.argv[2];
+
+if (args !== undefined) {
+  const argsValue = parseInt(args, 10);
+  if (!isNaN(argsValue)) {
+    console.log('My number ' + argsValue);
+  } else {
+    console.log('Not a number');
+  }
+} else {
+  console.log('Not a number');
+}
