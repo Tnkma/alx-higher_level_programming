7000ce6b719849f45f7289856f700e5716e8580c
Tnkma <onwusilikegodswill@gmail.com>
2024-01-10 14:56:40 +0100

    defined a function

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,17 +1,7 @@
 #!/usr/bin/node
-
-const args = process.argv.slice(2);
-
-if (args.length !== 2) {
-  console.log('NaN');
+function add(a, b) {
+  const c = a + b;
+  console.log(c);
 }
 
-const firstArg = parseInt(args[0], 10);
-const secArg = parseInt(args[1], 10);
-
-if (isNaN(firstArg) || isNaN(secArg)) {
-  console.log('NaN');
-} else {
-  const result = firstArg + secArg;
-  console.log(result);
-}
+add(Number(process.argv[2]), Number(process.argv[3]));
