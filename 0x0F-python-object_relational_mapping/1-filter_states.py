743c90eb73802129096d1eca6238f3ce1117793f
Tnkma <onwusilikegodswill@gmail.com>
2024-01-14 11:54:30 +0100

    hdhd

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,7 @@ if __name__ == " __main__ ":
             db=db_name
             )
     curs = connect_db.cursor()
-    curs.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY state.id")
+    curs.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
     selected_states = fetchall()
     for states in selected_states:
         print(states)
