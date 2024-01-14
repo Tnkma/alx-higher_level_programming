ba82636d474e6f72663642b227263f30ce8ba893
Tnkma <onwusilikegodswill@gmail.com>
2024-01-14 19:27:02 +0100

    create a Base class for the database

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-A python file that contains the class 
+A python file that contains the class
 definition of a State and an instance Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String
@@ -8,6 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
+
 
 class State(Base):
     """
