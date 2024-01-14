1ca0d62f2fb2908efb1390fa604172ee3e057d3f
Tnkma <onwusilikegodswill@gmail.com>
2024-01-14 11:50:20 +0100

    hdhd

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,22 +4,18 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    mysql_user = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    connect_db = MySQLdb.connect(
-        host='localhost',
+    db = MySQLdb.connect(
+        host="localhost",
         port=3306,
-        user=mysql_user,
-        passwd=mysql_password,
-        db=database_name
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
-    cursor = connect_db.cursor()
-    cursor.execute("SELECT * FROM states")
-    all_states =cursor.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    all_states = cur.fetchall()
     for each_state in all_states:
         print(each_state)
-    cursor.close()
+    cur.close()
     db.close()
---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+prints all state with a name starting with N
+"""
+import MySQLdb
+import sys
+
+if __name__ == " __main__ ":
+    user_name = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    connect_db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=user_name,
+            passwd=password,
+            db=db_name
+            )
+    curs = connect_db.cursor()
+    curs.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY state.id")
+    selected_states = fetchall()
+    for states in selected_states:
+        print(states)
+    curs.close()
+    db.close()
